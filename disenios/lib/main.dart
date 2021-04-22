324b2e701df208770183298c02ee4cd6bddd4da5
MarioMiranda98 <farolin95@msn.com>
2021-04-22 18:57:53 -0500

    first commit

---
+++ disenios/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'package:disenios/src/pages/headers_page.dart';
+ 
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Disenios App',
+      home: HeadersPage()
+    );
+  }
+}
